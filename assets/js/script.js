d14a1b1d1365408a5e2afa5c47f186c82a657790
C-dub <25037768+ColeS82@users.noreply.github.com>
2022-09-27 01:54:53 -0700

    hours change color dependant on the hour slot being in past, present, or future.  Save button save text input to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,113 @@
-$("#currentDay").text(moment().format("MMM Do, YYYY"));
+const button = document.querySelector('button')
+$("#currentDay").text(moment().format('dddd') + ", ");
+$("#dateTime").text(moment().format('MMMM Do YYYY', 'h:mm:ss a'));
+const hour = moment().hour()
+let text1 = document.querySelector('#text-1')
 
+console.log(hour)
+
+//current time highlighter
+if (hour === 9){
+        $('#time-9am, #text-1').addClass("text-bg-danger rounded");
+}
+
+if (hour === 10){
+        $('#time-10am, #text-2').addClass("text-bg-danger rounded");
+}
+
+if (hour === 11){
+        $('#time-11am, #text-3').addClass("text-bg-danger rounded");
+}
+
+if (hour === 12){
+        $('#time-12pm, #text-4').addClass("text-bg-danger rounded");
+}
+
+if (hour === 13){
+        $('#time-1pm, #text-5').addClass("text-bg-danger rounded");
+}
+
+if (hour === 14){
+        $('#time-2pm, #text-6').addClass("text-bg-danger rounded");
+}
+
+if (hour === 15){
+        $('#time-3pm, #text-7').addClass("text-bg-danger rounded");
+}
+
+if (hour === 16){
+        $('#time-4pm, #text-8').addClass("text-bg-danger rounded");
+}
+
+if (hour === 17){
+        $('#time-5pm, #text-9').addClass("text-bg-danger rounded");
+}
+
+//applying past styling for any row less that the current hour
+if (9 < hour){
+        $('#time-9am, #text-1').addClass("text-bg-secondary rounded");
+}
+if (10 < hour){
+        $('#time-10am, #text-2').addClass("text-bg-secondary rounded");
+}
+if (11 < hour){
+        $('#time-11am, #text-3').addClass("text-bg-secondary rounded");
+}
+if (12 < hour){
+        $('#time-12pm, #text-4').addClass("text-bg-secondary rounded");
+}
+if (13 < hour){
+        $('#time-1pm, #text-5').addClass("text-bg-secondary rounded");
+}
+if (14 < hour){
+        $('#time-2pm, #text-6').addClass("text-bg-secondary rounded");
+}
+if (15 < hour){
+        $('#time-3pm, #text-7').addClass("text-bg-secondary rounded");
+}
+if (16 < hour){
+        $('#time-4pm, #text-8').addClass("text-bg-secondary rounded");
+}
+if (17 < hour){
+        $('#time-5pm, #text-9').addClass("text-bg-secondary rounded");
+}
+
+//applying styling to future times greater than the current hour
+
+if (9 > hour){
+        $('#time-9am, #text-1').addClass("text-bg-success rounded");
+}
+if (10 > hour){
+        $('#time-10am, #text-2').addClass("text-bg-success rounded");
+}
+if (11 > hour){
+        $('#time-11am, #text-3').addClass("text-bg-success rounded");
+}
+if (12 > hour){
+        $('#time-12pm, #text-4').addClass("text-bg-success rounded");
+}
+if (13 > hour){
+        $('#time-1pm, #text-5').addClass("text-bg-success rounded");
+}
+if (14 > hour){
+        $('#time-2pm, #text-6').addClass("text-bg-success rounded");
+}
+if (15 > hour){
+        $('#time-3pm, #text-7').addClass("text-bg-success rounded");
+}
+if (16 > hour){
+        $('#time-4pm, #text-8').addClass("text-bg-success rounded");
+}
+if (17 > hour){
+        $('#time-5pm, #text-9').addClass("text-bg-success rounded");
+}
+
+button.addEventListener("click", storeText);
+
+
+function storeText(){
+        localStorage.setItem("text-1", text1.value)
+}
+
+let = textOutput = localStorage.getItem('text-1')
+text1.innerText = textOutput
